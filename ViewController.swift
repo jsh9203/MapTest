e2cb0e924287e61981bae01b3008a9f016aa6402
jsh9203 <30308255+jsh9203@users.noreply.github.com>
2017-07-20 14:09:24 +0900

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by 주수현 on 2017. 7. 6..
+//  Copyright © 2017년 주수현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    @IBAction func actionChangeLabel() {
+        label.text = "버튼을눌렀다!!!"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
